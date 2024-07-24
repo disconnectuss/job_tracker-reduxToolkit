73990c0479665b744952d656f5722a361772a61c
Fatma A <fka201520@gmail.com>
2024-07-24 04:54:09 +0300

    json file created

--- job_tracker/src/pages/JobList.jsx
+++ job_tracker/src/pages/JobList.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import JobCard from "../components/JobCard";
+import axios from "axios";
 
 const JobList = () => {
+  const [jobs, setJobs] = useState(null)
+  useEffect (()=>{
+    axios
+    .get('http://localhost:3030/jobs')
+    .then((res)=> setJobs(res.data))
+  }, [])
+  // console.log(jobs)
   return (
     <div>
       <Header/>
+      <h3 className="count">You are viewing (5) out of (10) available jobs. </h3>
       <div className="list_section">
         <JobCard />
         <JobCard />
